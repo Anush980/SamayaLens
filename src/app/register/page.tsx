66486d0322e5cbabea01bb75a597b1be817792c2
Anush980 <anush.stha980@outlook.com>
2025-06-05 11:37:05 +0545

    made register and login Ui

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,6 @@
 'use client'; 
 
-import {RegisterForm} from '@components/RegisterForm'
+import {RegisterForm} from '@/components/register-form'
 import React from 'react';
 
 export default function register (){
